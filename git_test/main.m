eb9a7f354f37dea5e8c2da07653b17db9792e602
horizon <891873395@qq.com>
2016-02-17 22:51:32 +0800

    Initial Commit

---
+++ git_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  git_test
+//
+//  Created by horizon on 16/2/17.
+//  Copyright © 2016年 horizon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
